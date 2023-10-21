2049af23ab1dba14c5fa5b868f3c52d36a98b9ad
amit3671 <127406102+amit3671@users.noreply.github.com>
2023-10-21 18:00:59 +0530

    Update admin details.sql

--- Registration/src/main/resources/admin details.sql
+++ Registration/src/main/resources/admin details.sql
@@ -3,5 +3,5 @@
 INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_GUEST'); 
 
 INSERT INTO `users` VALUES (1,'admin','admin','admin@gl.com','admin','admin','admin','$2a$10$k9644mshajjDvMhU8p76.u4sgOFuINZDkZ/csNgzFY99W1diZjBuC'); 
- 
+ --Password : admin--
 INSERT INTO `users_roles` VALUES (1,1);
